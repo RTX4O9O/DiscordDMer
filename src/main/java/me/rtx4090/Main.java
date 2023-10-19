1785cf47aeeddc4a571eac9792843e68aae47f1b
JayKuoNoob <jaykuo1203@gmail.com>
2023-10-20 02:03:01 +0800

    commit

--- src/main/java/me/rtx4090/Main.java
+++ src/main/java/me/rtx4090/Main.java
@@ -4,12 +4,16 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.entities.User;
+import java.util.Scanner;
 
 public class Main extends ListenerAdapter {
     private JDA jda;
 
     public Main() {
-        String botToken = "MTE1Nzg5NjQwNDczNjQyMTg5OQ.Gq4mVV.pU4M0FId1BTwAn-BPi5j8e81d5zfaR1AwR7fWA";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Discord Bot Token: ");
+        String botToken = scanner.nextLine();
         jda = JDABuilder.createDefault(botToken).addEventListeners(this).build();
     }
 
@@ -20,25 +24,28 @@ public class Main extends ListenerAdapter {
     @Override
     public void onGuildReady(GuildReadyEvent event) {
         System.out.println("All members in the guild " + event.getGuild().getName() + " have been downloaded.");
-
-        // Now you can be sure that all server members are loaded
-        sendPrivateMessage();
+        String quitStr;
+        do{
+            sendPrivateMessage();
+            Scanner quit = new Scanner(System.in);
+            quitStr = quit.nextLine();
+        } while (!quitStr.equals("!quit"));
     }
 
     private void sendPrivateMessage() {
-        String recipientUserId = "860877200823025664"; // Replace with the actual user ID
-        String messageContent = "L";
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("User ID: ");
+        String recipientUserId = scanner.nextLine();
 
-        User recipient = jda.getUserById(recipientUserId);
-        System.out.println(recipient);
+        System.out.print("Message: ");
+        String messageContent = scanner.nextLine();
 
-        if (recipient != null) {
-            recipient.openPrivateChannel().queue(privateChannel -> {
-                privateChannel.sendMessage(messageContent).queue();
-                System.out.println("Message has been sent to " + recipient.getAsTag() + " successfully");
-            });
-        } else {
-            System.out.println("Can't find the user. Please make sure you've used the correct ID");
-        }
+        jda.openPrivateChannelById(recipientUserId).queue(channel -> {
+            User user = channel.getUser();
+            channel.sendMessage(messageContent).queue();
+            System.out.printf("Message has been sent to %#s successfully\n", user);
+        });
+
     }
+
 }
